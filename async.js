842b402c7e0cf8b01ecfb0c14aa1925778d718a1
rungkung <rungnan008@gmail.com>
2017-11-21 10:31:33 +0700

    test

--- async.js
+++ async.js
@@ -1,7 +1,9 @@
 const request = require('request');
 
 const test = () => {
-  
+  request('http://www.google.com',(error, response, body)=>{
+    console.log('Response');
+  });
 }
 //START OF PROGRAM
 console.log("Hitting google");
